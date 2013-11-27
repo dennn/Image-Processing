133057a10fc3009582d2b831178d9741ab69d4d0
Fernando Mussel <fernandomussel91@gmail.com>
2013-11-27 15:18:35 +0000

    Updated run script and new Makefile

--- Object Detection/run.sh
+++ Object Detection/run.sh
@@ -2,10 +2,16 @@ j=0
 #for ((i =0; i< 12;i++));do
 #./a.out images/obama$i.jpg ; mv output.jpg outputs/obama$i.jpg
 #done
-t=30
+if [[ $# -ne 1 ]]; then
+	t=30
+else
+	t=$1
+fi
 #t=$1
 echo "threshold: $t"
 for i in `ls dartboards/dart*.jpg`; do
 new_name=$(basename $i)
-./a.out $i $t ; mv output.jpg dart_out/${new_name}
+output=$(./a.out $i $t ; mv output.jpg dart_out/${new_name})
+filename=$(basename $i)
+echo "$filename; $output"
 done
