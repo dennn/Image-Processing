b33ca37aee0a6400b31328c654d973af397bbdf0
Adnan Abdulhussein <adnan@prydoni.us>
2013-11-27 15:41:32 +0000

    back projection code

--- Object Detection/face.cpp
+++ Object Detection/face.cpp
@@ -11,6 +11,7 @@ using namespace cv;
 
 /** Function Headers */
 void detectAndSave( Mat frame, double );
+void Hist_and_Backproj(int, void* );
 
 /** Global variables */
 String logo_cascade_name = "dartcascade.xml";
@@ -96,21 +97,84 @@ void detect_circle (Mat& frame, Mat& frame_gray, std::vector<Rect>& boards, doub
 
 	
 }
+
+Mat src; Mat hsv; Mat hue; Mat test; Mat hsv_test; Mat hue_test;
+int bins = 25;
 /** @function main */
 int main( int argc, const char** argv )
 {
-	CvCapture* capture;
-	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	//Mat frame = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	// CvCapture* capture;
+	// Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	// //Mat frame = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
-	//detect_circle (frame);
-	//-- 1. Load the cascades
-	//logo_cascade.load(logo_cascade_name);
-	if( !logo_cascade.load( logo_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+	// //detect_circle (frame);
+	// //-- 1. Load the cascades
+	// //logo_cascade.load(logo_cascade_name);
+	// if( !logo_cascade.load( logo_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
-	detectAndSave( frame, atof(argv[2]) );
+	// detectAndSave( frame, atof(argv[2]) );
+
+	/// Read the image
+	src = imread( argv[1], 1 );
+	test = imread(argv[2], 1);
+	/// Transform it to HSV
+	cvtColor( src, hsv, CV_BGR2HSV );
+	cvtColor( test, hsv_test, CV_BGR2HSV );
+
+	/// Use only the Hue value
+	hue.create( hsv.size(), hsv.depth() );
+	int ch[] = { 0, 0 };
+	mixChannels( &hsv, 1, &hue, 1, ch, 1 );
+
+	hue_test.create( hsv_test.size(), hsv_test.depth() );
+	mixChannels( &hsv_test, 1, &hue_test, 1, ch, 1 );
+
+	/// Create Trackbar to enter the number of bins
+	char* window_image = "Source image";
+	namedWindow( window_image, CV_WINDOW_AUTOSIZE );
+	createTrackbar("* Hue  bins: ", window_image, &bins, 180, Hist_and_Backproj );
+	Hist_and_Backproj(0, 0);
+
+	/// Show the image
+	imshow( window_image, src );
+
+	/// Wait until user exits the program
+	waitKey(0);
 
 	return 0;
+}
+
+/**
+ * @function Hist_and_Backproj
+ * @brief Callback to Trackbar
+ */
+void Hist_and_Backproj(int, void* )
+{
+  MatND hist;
+  int histSize = MAX( bins, 2 );
+  float hue_range[] = { 0, 180 };
+  const float* ranges = { hue_range };
+
+  /// Get the Histogram and normalize it
+  calcHist( &hue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
+  normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
+
+  /// Get Backprojection
+  MatND backproj;
+  calcBackProject( &hue_test, 1, 0, hist, backproj, &ranges, 1, true );
+
+  /// Draw the backproj
+  imshow( "BackProj", backproj );
+
+  /// Draw the histogram
+  int w = 400; int h = 400;
+  int bin_w = cvRound( (double) w / histSize );
+  Mat histImg = Mat::zeros( w, h, CV_8UC3 );
+
+  for( int i = 0; i < bins; i ++ )
+     { rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ), Scalar( 0, 0, 255 ), -1 ); }
+
+  imshow( "Histogram", histImg );
 }
 
 /** @function detectAndSave */
