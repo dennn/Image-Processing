ea6cf282ec31453a682e91e0bc84f25787d9412c
Fernando Mussel <fernandomussel91@gmail.com>
2013-11-27 15:26:15 +0000

    Best parameters

--- Object Detection/face.cpp
+++ Object Detection/face.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 
 /** Function Headers */
 void detectAndSave( Mat frame, double );
-
+void threshold_image (Mat&, int);
 /** Global variables */
 String logo_cascade_name = "dartcascade.xml";
 
@@ -34,13 +34,6 @@ void detect_circle (Mat& frame, Mat& frame_gray, std::vector<Rect>& boards, doub
   int ddepth = CV_16S;
 	//cvtColor( frame, frame_gray, CV_BGR2GRAY );
 //	Mat frame = imread(image, CV_GRAY_SCALE);
-//
-//
-//
-//
-//
-//
-//
 	Mat grad_x, grad_y;
 	  Mat abs_grad_x, abs_grad_y;
 		char* window_name = "Gradient image";
@@ -57,12 +50,18 @@ void detect_circle (Mat& frame, Mat& frame_gray, std::vector<Rect>& boards, doub
 
 	  /// Total Gradient (approximate)
 	  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-	namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-	//imshow(window_name, grad);
-	//waitKey(0);
 
-	HoughCircles( grad, circles, CV_HOUGH_GRADIENT, 1, frame.rows/10, 200, 100, 10, 200 );
+	threshold_image (grad, threshold); 
+#ifdef STEP
+	namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+	imshow(window_name, grad);
+	waitKey(0);
+#endif
+	HoughCircles( grad, circles, CV_HOUGH_GRADIENT, 1, grad.rows/8, 200, 100, 10, 200 );
 	
+	std::cout << "Classifier: " << boards.size() << "; hough_circle: " <<\
+	   	circles.size () << std::endl;
+#ifndef FINAL
 	  for( size_t i = 0; i < circles.size(); i++ )
   	{
       	Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
@@ -72,8 +71,8 @@ void detect_circle (Mat& frame, Mat& frame_gray, std::vector<Rect>& boards, doub
       	// circle outline
       	circle( frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
    	}
-	
-/*
+#endif
+	//	
 	for (int i = 0; i < circles.size (); i++){
 		for (int j = 0; j < boards.size (); j++){
 //			dist = pow((circles[i][0] - boards[j][0]), 2) 
@@ -88,10 +87,9 @@ void detect_circle (Mat& frame, Mat& frame_gray, std::vector<Rect>& boards, doub
 			}
 		}
 	}
-*/	
-//	if ( boards.size () > 0 ){
-//		rectangle(frame, Point(boards[min_index].x, boards[min_index].y), Point(boards[min_index].x + boards[min_index].width, boards[min_index].y + boards[min_index].height), Scalar( 0, 255, 0 ), 2);
-//	}
+	if ( boards.size () > 0 ){
+		rectangle(frame, Point(boards[min_index].x-5, boards[min_index].y-5), Point(boards[min_index].x + boards[min_index].width + 5, boards[min_index].y + boards[min_index].height + 5), Scalar( 255, 255, 0 ), 2);
+	}
 	//imwrite( "output.jpg", frame );
 
 	
@@ -101,8 +99,14 @@ int main( int argc, const char** argv )
 {
 	CvCapture* capture;
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	//Mat frame = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat frame_gray = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
+#ifdef GRAY_SCALE
+	namedWindow( "Gray scale", CV_WINDOW_AUTOSIZE );
+	imshow("Gray scale", frame_gray);
+	waitKey(0);
+	exit(0);
+#endif
 	//detect_circle (frame);
 	//-- 1. Load the cascades
 	//logo_cascade.load(logo_cascade_name);
@@ -124,17 +128,38 @@ void detectAndSave( Mat frame, double threshold )
 	//equalizeHist( frame_gray, frame_gray );
 
 	//-- Detect faces
-	logo_cascade.detectMultiScale( frame_gray, faces, 1.1, 3, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
-	std::cout << faces.size() << std::endl;
+	logo_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, 
+			Size(50, 50), Size(500,500) );
 	//detect_circle (frame_gray, faces, threshold);
 	detect_circle (frame,frame_gray, faces, threshold);
+#ifndef FINAL
 	for( int i = 0; i < faces.size(); i++ )
 	{
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
-
+#endif
 
 	//-- Save what you got
 	imwrite( "output.jpg", frame );
+}
 
+
+void threshold_image(Mat &magnitude, int threshold)
+{
+    for ( int i = 0; i < magnitude.rows; i++ )
+    {
+        for( int j = 0; j < magnitude.cols; j++ )
+        {
+/*
+            if (magnitude.at<uchar>( i, j ) > threshold)
+                magnitude.at<uchar>( i, j ) = 255;
+            else
+                magnitude.at<uchar>( i, j ) = 0;
+*/
+
+            if (magnitude.at<uchar>( i, j ) < threshold)
+                magnitude.at<uchar>( i, j ) = 0;
+        }
+    }
+    
 }
