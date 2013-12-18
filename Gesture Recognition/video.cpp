9e58fb4ac0b6a275f15f89950613328ed88205af
Fernando Mussel <fernandomussel91@gmail.com>
2013-12-18 16:43:07 +0000

    getWindow, LKTracker tested and working.

--- Gesture Recognition/video.cpp
+++ Gesture Recognition/video.cpp
@@ -34,6 +34,7 @@ void LKTracker (const cv::Mat& Dx, const cv::Mat& Dy, const cv::Mat& Dt,
 		cv::Mat& b);
 void LK (cv::Mat& prevFrame, cv::Mat& frame,
 		priority_queue<line_segment,vector<line_segment>, compare_line_segments>&, int);
+void getWindow (const cv::Mat& frame,cv::Mat& window, cv::Point start, int windowDimension);
 void print_mat (cv::Mat& m);
 void print_mat_d (cv::Mat& m);
 
@@ -98,18 +99,14 @@ int main( int argc, const char** argv )
 
 		//Checks whether we should the current frame to get the Derivative images or
 		//wait for another one
-		if (count >= sampling){
-			count = -1;
-			LK (prevFrame, frame_gray, of_vec_queue, OF_VEC_NUM);
-			while (of_vec_queue.size() > 0){
-				temp = of_vec_queue.top();
-				cv::line(frame, temp.start, temp.end, Scalar(255,0,0)); 
-				of_vec_queue.pop();
-			}
-			imshow("video", frame);
-			frame_gray.copyTo(prevFrame);
+		LK (prevFrame, frame_gray, of_vec_queue, OF_VEC_NUM);
+		while (of_vec_queue.size() > 0){
+			temp = of_vec_queue.top();
+			cv::line(frame, temp.start, temp.end, Scalar(255,0,0)); 
+			of_vec_queue.pop();
 		}
-		count ++;
+		imshow("video", frame);
+		frame_gray.copyTo(prevFrame);
 	}
 }
 
@@ -144,42 +141,6 @@ void calcMatA (const cv::Mat& Dx, const cv::Mat& Dy, const cv::Point& targetPoin
 		unsigned windowDimension, cv::Mat& A){
 	Point windowStart (targetPoint.x - windowDimension/2, targetPoint.y - windowDimension/2);
 	int end_row = windowStart.y + windowDimension;
-	int sum_x = 0;
-	int sum_y = 0;
-	int sum_t = 0;
-	//Get Ix(sum_x) first
-	for ( int i = windowStart.y ; i < end_row; i++){
-		const float* current_row = Dx.ptr<float>(i);
-		current_row+= windowStart.x;
-		for (unsigned j = 0; j < windowDimension; j++,current_row++){
-			//adds the value from the pixel (windowStart.y+i, windowStart.x+j)
-			sum_x += *current_row;
-		}
-	}
-
-	//Calc sum_y
-	for ( int i = windowStart.y ; i < end_row; i++){
-		const float* current_row = Dy.ptr<float>(i);
-		current_row+= windowStart.x;
-		for (unsigned j = 0; j < windowDimension; j++,current_row++){
-			//adds the value from the pixel (windowStart.y+i, windowStart.x+j)
-			sum_y += *current_row;
-		}
-	}
-	A.at<float>(0,0) = sum_x*sum_x;
-	A.at<float>(0,1) = sum_x*sum_y;
-	A.at<float>(1,0) = sum_x*sum_y;
-	A.at<float>(1,1) = sum_y*sum_y;
-
-}
-void LKTracker (const cv::Mat& Dx, const cv::Mat& Dy, const cv::Mat& Dt, 
-		const cv::Point& targetPoint, unsigned windowDimension, cv::Mat& A, 
-		cv::Mat& b){
-	//OBS: Dx, Dy and Dt HAVE HAD BEEN PADDED ACCORDINGLY, i. e. WITH THE
-	//APPROPRIATE SIZE DEPENDING ON THE DIMENSION OF THE NEIGHBOURHOOD WINDOW
-
-	Point windowStart (targetPoint.x - windowDimension/2, targetPoint.y - windowDimension/2);
-	int end_row = windowStart.y + windowDimension;
 	float sum_x = 0;
 	float sum_y = 0;
 	float sum_t = 0;
@@ -207,59 +168,98 @@ void LKTracker (const cv::Mat& Dx, const cv::Mat& Dy, const cv::Mat& Dt,
 	A.at<float>(1,0) = sum_x*sum_y;
 	A.at<float>(1,1) = sum_y*sum_y;
 
-	//Calc sum_t
-	for ( int i = windowStart.y ; i < end_row; i++){
-		const float* current_row = Dt.ptr<float>(i);
-		current_row+= windowStart.x;
-		for (unsigned j = 0; j < windowDimension; j++,current_row++){
-			//adds the value from the pixel (windowStart.y+i, windowStart.x+j)
-			sum_t += *current_row;
-		}
-	}
-	//This does not make sense at all. The matrix b is declared as having to elements in
-	//the first dimension. However, here we the access is inverted.
+}
+void LKTracker (const cv::Mat& Dx, const cv::Mat& Dy, const cv::Mat& Dt, 
+		const cv::Point& windowStart, unsigned windowDimension, cv::Mat& A, 
+		cv::Mat& b){
+
+	cv::Mat window;
+	float sum_x, sum_y, sum_t;
+
+
+	window.create(windowDimension, windowDimension, CV_32F);
+
+	getWindow(Dx, window, windowStart, windowDimension);
+	sum_x = sum(window)[0];
+	std::cout << "window_x:" << window << std::endl;
+	std::cout << "sum_x:" << sum_x << std::endl;
+	
+	getWindow(Dy, window, windowStart, windowDimension);
+	sum_y = sum(window)[0];
+	std::cout << "window_y:" << window << std::endl;
+	std::cout << "sum_y:" << sum_y << std::endl;
+	getWindow(Dt, window, windowStart, windowDimension);
+	sum_t = sum(window)[0];
+	std::cout << "window_t:" << window << std::endl;
+	std::cout << "sum_t:" << sum_t << std::endl;
+
+	A.at<float>(0,0) = sum_x*sum_x;
+	A.at<float>(0,1) = sum_x*sum_y;
+	A.at<float>(1,0) = sum_x*sum_y;
+	A.at<float>(1,1) = sum_y*sum_y;
+
 	b.at<float>(0,0) = (-1)*sum_x*sum_t;
 	b.at<float>(1,0) = (-1)*sum_y*sum_t;
 }
 void LK (cv::Mat& prevFrame, cv::Mat& frame,
 		priority_queue<line_segment,vector<line_segment>, compare_line_segments>& vec_queue, 
 		int max_vec_num){
+
+
+
 	cv::Mat derivX, derivY, derivT;
 	cv::Mat padDerivX, padDerivY, padDerivT;
 	cv::Mat vX, vY;
 	cv::Mat A, AInv, b, v;
 	int vec_num = 0;
-	int window_i, window_j;
 
 
 	A.create(2,2,CV_32F);
 	b.create(2,1,CV_32F);
-	int windowDimension = 11;
+	int windowDimension = 2;
 	int window_radius = windowDimension / 2;
-	vX.create(frame.rows / window_radius, frame.cols/window_radius,CV_32F);
-	vY.create(frame.rows / window_radius, frame.cols/window_radius,CV_32F);
 
 	line_segment seg_vec;
 
-	cv::Point targetPoint;
+	cv::Point windowStart;
+	cv::Point windowCenter;
 	cv::Point velEndPoint;
+	int window_i_size = frame.rows / windowDimension;
+	int window_j_size = frame.cols / windowDimension;
+	vX.create(window_j_size, window_i_size,CV_32F);
+	vY.create(window_j_size, window_i_size,CV_32F);
 
 	getDerivatives (prevFrame, frame, derivX, derivY, derivT);
 	//We need to pad derivX, derivY and derivT
 
-
-	window_i = 0;
+	cv::Mat window;
+	window.create (windowDimension, windowDimension, CV_32F);
 	//Calculates the optical flow for every single pixel in frame
-	for (int i = window_radius; i < frame.rows - window_radius; i+=window_radius, window_i++){
-		window_j = 0;
-		for (int j = window_radius; j < frame.cols - window_radius ; j+=window_radius,window_j++){
-			//TODO: I think this logic is working: x = j and y = i
+	for (int window_i = 0; window_i < window_i_size; window_i++){
+		for (int window_j = 0; window_j < window_j_size; window_j++){
+			windowStart.x = (window_j*windowDimension);
+			windowStart.y = (window_i*windowDimension);
+
+			windowCenter.x = windowStart.x + window_radius;
+			windowCenter.y = windowStart.y + window_radius;
+			cv::Mat x, y, t;
+
+			getWindow (frame, x, Point(5,0), 5);
+			getWindow (frame, y, Point(7,0), 5);
+			getWindow (frame, t, Point(0,0), 5);
+
+			std::cout << "Dx:" << x << std::endl;
+			std::cout << "Dy:" << y << std::endl;
+			std::cout << "Dt:" << t << std::endl;
 			
+			LKTracker (x,y,t,Point(3,3),2, A, b);
+			std::cout << "A:" << A << std::endl;
+			std::cout << "b:" << b << std::endl;
 
-			targetPoint.x = j;
-			targetPoint.y = i;
+			exit (0);
 
-			LKTracker (derivX, derivY, derivT, targetPoint, windowDimension, A, b);
+			/*
+			LKTracker (derivX, derivY, derivT, windowStart, windowDimension, A, b);
 
 			//Now we calculate the optical flow velocity vector v	
 			if ( determinant (A) == 0) {
@@ -274,58 +274,43 @@ void LK (cv::Mat& prevFrame, cv::Mat& frame,
 			//original image, base point of the velocity vector, end of the velocity vector
 
 			seg_vec.norm = norm(v);
-/*
-			if (seg_vec.norm == 0 ){
-				vX.at<float>(window_i, window_j) = 0;
-				vY.at<float>(window_i, window_j) = 0;
-				continue;
-			}
-			*/
 			vX.at<float>(window_i, window_j) = v.at<float>(0,0);
 			vY.at<float>(window_i, window_j) = v.at<float>(0,1);
+			std::cout << "A:" << A << std::endl;
+			std::cout << "InvA:" << AInv << std::endl;
+			std::cout << "b:" << b << std::endl;
 
-			velEndPoint.x = (targetPoint.x + v.at<float>(0,0));
-			velEndPoint.y = (targetPoint.y + v.at<float>(0,1));
-			seg_vec.start = targetPoint;
+			std::cout << "v:" << v << std::endl;
+
+			windowCenter.x = windowStart.x + window_radius;
+			windowCenter.y = windowStart.y + window_radius;
+
+			velEndPoint.x = (windowCenter.x + v.at<float>(0,0));
+			velEndPoint.y = (windowCenter.y + v.at<float>(0,1));
+			seg_vec.start = windowCenter;
 			seg_vec.end = velEndPoint;
 
 			vec_queue.push(seg_vec);
-			
-			/*
-			if (vec_num < max_vec_num){
-				velEndPoint.x = (targetPoint.x + v.at<float>(0,0));
-				velEndPoint.y = (targetPoint.y + v.at<float>(0,1));
-				seg_vec.start = targetPoint;
-				seg_vec.end = velEndPoint;
-
-				vec_queue.push(seg_vec);
-				vec_num++;
-			}
-			else{
-				if (seg_vec.norm > vec_queue.top().norm){
-					velEndPoint.x = (targetPoint.x + v.at<float>(0,0));
-					velEndPoint.y = (targetPoint.y + v.at<float>(0,1));
-					seg_vec.start = targetPoint;
-					seg_vec.end = velEndPoint;
-
-					vec_queue.push(seg_vec);
-					vec_num++;
-				}
-			}*/
-
+			*/
 		}
 	}	
+	std::cout << "\n\n\n\n\nNEW FRAME\n" ;
+	exit(0);
 
-
-//	cv::normalize(vX, vX, 0, 255, CV_MINMAX);
-//	cv::normalize(vY, vY, 0, 255, CV_MINMAX);
-	//std::cout << "vX:" << vX << std::endl;
-	//std::cout << "vY:" << vY << std::endl;
 	imshow("vX", vX);
 	imshow("vY", vY);
 }
 
 
+void getWindow (const cv::Mat& frame,cv::Mat& window, cv::Point start, int windowDimension){
+	//FIRST COORDINATE IN cv::Mat::at FUNCTION IS THE INDEX OF THE LINE!!!!!!!!!!
+	window.create (windowDimension, windowDimension, CV_32F);
+	for (int row = 0; row < windowDimension; row++){
+		for (int col = 0; col < windowDimension; col++){
+			window.at<float>(row,col) = frame.at<float>(row+start.y,col+start.x);
+		}
+	}
+}
 
 /////////HARRIS CORNER DETECTOR/////////////////////////
 
